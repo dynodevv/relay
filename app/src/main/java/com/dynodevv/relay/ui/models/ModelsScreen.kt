d31f1f65ba17b7f37b8f7c1a4d51005287cda2ad
Dyno <85120286+dynodevv@users.noreply.github.com>
2026-05-06 20:19:16 +0200

    Fix build: add missing Box and size imports

--- app/src/main/java/com/dynodevv/relay/ui/models/ModelsScreen.kt
+++ app/src/main/java/com/dynodevv/relay/ui/models/ModelsScreen.kt
@@ -2,6 +2,7 @@ package com.dynodevv.relay.ui.models
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
